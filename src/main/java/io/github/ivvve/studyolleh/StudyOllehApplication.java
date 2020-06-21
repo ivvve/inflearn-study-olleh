df2a8bee53bc939356238bb11a53203ec81d1a43
devson <sonyc5720@gmail.com>
2020-06-21 21:48:28 +0900

    init project

---
+++ src/main/java/io/github/ivvve/studyolleh/StudyOllehApplication.java
@@ -0,0 +1,13 @@
+package io.github.ivvve.studyolleh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOllehApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyOllehApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ivvve/studyolleh/StudyOllehApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ivvve.studyolleh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyOllehApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
